2dbb91b68d218df459e01195430c8285067feedb
Alexandr Zemtsov <a.zemcov@gmail.com>
2024-04-07 12:47:33 +0300

    мигание и база данных

--- src/components/Line/Line.jsx
+++ src/components/Line/Line.jsx
@@ -2,31 +2,18 @@ import { useState } from "react";
 import QuestionButton from "/src/components/QuestionButton/QuestionButton.jsx";
 import classes from "/src/components/Line/Line.module.css";
 import { round1Questions } from "/data.js";
-import ModalQuestion from "/src/components/modal/ModalQuestion.jsx";
 
 export default function Line({ children, lineN }) {
-  let [isModalOpen, setIsModalOpen] = useState(false);
-  let [actualQuestion, setActualQuestion] = useState("");
-
   return (
     <div className={classes.line}>
       <div className={classes.normalTheme}>
         <p className={classes.themeText}>{children}</p>
       </div>
       {round1Questions[lineN].line.map((_, i) => (
-        <QuestionButton
-          onClick={() => {
-            setIsModalOpen(true);
-            setActualQuestion(round1Questions[lineN].line[i].question);
-          }}
-        >
+        <QuestionButton lineN={lineN} columnN={i}>
           {round1Questions[lineN].line[i].price}
         </QuestionButton>
       ))}
-      <ModalQuestion open={isModalOpen}>
-        <p>{actualQuestion}</p>
-        <button onClick={() => setIsModalOpen(false)}>закрыть</button>
-      </ModalQuestion>
     </div>
   );
 }
--- src/components/QuestionButton/QuestionButton.jsx
+++ src/components/QuestionButton/QuestionButton.jsx
@@ -1,10 +1,37 @@
+import { useState } from "react";
 import { Children } from "react";
+import ModalQuestion from "/src/components/modal/ModalQuestion.jsx";
+import { round1Questions } from "/data.js";
 import classes from "/src/components/QuestionButton/QuestionButton.module.css";
 
-export default function QuestionButton({ children, ...props }) {
+export default function QuestionButton({ children, lineN, columnN }) {
+  let [isModalOpen, setIsModalOpen] = useState(false);
+  let [question, setQuestion] = useState("");
+  let [isBlinking, setIsBlinking] = useState(false);
+
+  function flash() {
+    setIsBlinking(true);
+    setTimeout(() => {
+      setIsBlinking(false);
+      setIsModalOpen(true);
+      setQuestion(round1Questions[lineN].line[columnN].question);
+    }, 700);
+  }
+
   return (
-    <button {...props} className={classes.normalButton}>
-      {children}
-    </button>
+    <>
+      <button
+        onClick={flash}
+        className={`${classes.normalButton} ${
+          isBlinking ? classes.blinking : ""
+        }`}
+      >
+        {children}
+      </button>
+      <ModalQuestion open={isModalOpen}>
+        <p>{question}</p>
+        <button onClick={() => setIsModalOpen(false)}>закрыть</button>
+      </ModalQuestion>
+    </>
   );
 }
