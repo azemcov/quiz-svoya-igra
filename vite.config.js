fc547439024b162d8bd7715d5d9bb2e2674443e3
Alexandr Zemtsov <a.zemcov@gmail.com>
2024-09-23 21:32:38 +0300

    vite.config.js fix

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,37 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // потому, что будет на github в репозитории "SvoyaIgra_JS"
-  base: "/SvoyaIgra_JS/",
+  // потому, что будет на github в репозитории "quiz_Svoya_Igra"
+  base: '/quiz_Svoya_Igra/',
 
   plugins: [react()],
 
   //подключаем пути-псевдонимы
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@fonts": path.resolve(__dirname, "./public/fonts"),
-      "@images": path.resolve(__dirname, "./public/images"),
-      "@sounds": path.resolve(__dirname, "./public/sounds"),
-      "@media": path.resolve(__dirname, "./public/media"),
+      '@': path.resolve(__dirname, './'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@fonts': path.resolve(__dirname, './public/fonts'),
+      '@images': path.resolve(__dirname, './public/images'),
+      '@sounds': path.resolve(__dirname, './public/sounds'),
+      '@media': path.resolve(__dirname, './public/media'),
     },
   },
 
   // потому, хз почему. Чтобы работала загрузка данных из data.js
   assetsInclude: [
-    "**/*.JPG",
-    "**/*.jpg",
-    "**/*.jpeg",
-    "**/*.png",
-    "**/*.gif",
-    "**/*.webp",
-    "**/*.svg",
-    "**/*.mp3",
-    "**/*.wav",
-    "**/*.ogg",
-    "**/*.aac",
+    '**/*.JPG',
+    '**/*.jpg',
+    '**/*.jpeg',
+    '**/*.png',
+    '**/*.gif',
+    '**/*.webp',
+    '**/*.svg',
+    '**/*.mp3',
+    '**/*.wav',
+    '**/*.ogg',
+    '**/*.aac',
   ],
 });
